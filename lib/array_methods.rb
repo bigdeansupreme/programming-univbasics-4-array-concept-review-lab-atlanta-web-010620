49c97e33e67e7a79c88273b45ac9fd934d7a5b4b
Dino Kunovac <dinokunovac@gmail.com>
2019-12-25 19:03:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,10 +1,10 @@
 def find_element_index(array, value_to_find)
   array.length.times do |counter|
     if array[counter] == value_to_find
-      return 
-  
-  
-  
+      return counter
+    end
+    nil
+
 end
 
 def find_max_value(array)
