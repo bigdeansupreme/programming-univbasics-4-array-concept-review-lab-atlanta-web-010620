9e0c10c4113462d5ed2cfc17724ba2905202380a
Dino Kunovac <dinokunovac@gmail.com>
2019-12-25 19:22:55 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |counter|
+  array.length.times { |counter|
     if array[counter] == value_to_find
       return counter
     end
-  end
+  }
   nil
 end
 
